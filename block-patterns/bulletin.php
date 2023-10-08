bc4c5a17f439b541cec15816587c991dff9ace48
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-10-08 11:46:50 +0600

    en to fr Newsletter → Bulletin

--- block-patterns/bulletin.php
+++ block-patterns/bulletin.php
@@ -1,20 +1,21 @@
 <?php
 /*
- * Block Pattern - Newsletter
+ * Block Pattern - Bulletin bulletin
  */
 
-function insg_register_newsletter_block_pattern()
+function insg_register_bulletin_block_pattern()
 {
 
 	$content = <<<CONTENT
 	
 			<!-- wp:generateblocks/container {"uniqueId":"2f3facc4","backgroundColor":"var(\u002d\u002dinsg-black)","isDynamic":true,"blockVersion":4,"spacing":{"paddingTop":"32px","paddingLeft":"32px","paddingRight":"128px","paddingBottom":"32px","marginTop":"","paddingTopMobile":"16px","paddingLeftMobile":"16px","paddingRightMobile":"16px","paddingBottomMobile":"16px"},"className":"insg_block_pattern"} -->
 	<!-- wp:generateblocks/headline {"uniqueId":"3a96d841","element":"div","blockVersion":3,"typography":{"fontSize":"45px","fontWeight":"700","fontFamily":"Neue Haas Grotesk Display Pro","lineHeight":"55px","fontSizeMobile":"30px","lineHeightMobile":"1.1em"},"textColor":"var(\u002d\u002dinsg-white)"} -->
-	<div class="gb-headline gb-headline-3a96d841 gb-headline-text">Become a <mark style="background-color:var(--insg-blue)" class="has-inline-color has-insg-white-color">smarter</mark><br>marketer for 0$</div>
+	<div class="gb-headline gb-headline-3a96d841 gb-headline-text"><mark style="background-color:var(--insg-blue)" class="has-inline-color has-insg-white-color">Guides</mark><br>sur les Créateurs
+	Meilleurs Outils Sociaux</div>
 	<!-- /wp:generateblocks/headline -->
 
 	<!-- wp:paragraph {"style":{"typography":{"fontSize":"17px"}},"textColor":"insg-white"} -->
-	<p class="has-insg-white-color has-text-color" style="font-size:17px">Get our 5 min <strong>weekly newsletter </strong>on what matters on Social.</p>
+	<p class="has-insg-white-color has-text-color" style="font-size:17px">Recevez nos <strong>meilleurs guides </strong>et outils sur les Créateurs de Contenu & Marques sur les réseaux sociaux livrés chaque semaine.</p>
 	<!-- /wp:paragraph -->
 
 	<!-- wp:html -->
@@ -27,8 +28,8 @@ function insg_register_newsletter_block_pattern()
 	<figure class="gb-block-image gb-block-image-e4c330fb"><img class="gb-image gb-image-e4c330fb" src="/wp-content/uploads/marketer-people.png" alt="Marketer People"/></figure>
 	<!-- /wp:generateblocks/image -->
 
-	<!-- wp:generateblocks/headline {"uniqueId":"703a9bab","element":"p","blockVersion":3,"typography":{"fontSize":"16px","fontSizeMobile":"15px"},"textColor":"var(\u002d\u002dinsg-white)"} -->
-	<p class="gb-headline gb-headline-703a9bab gb-headline-text">+17k people already enjoyed it</p>
+	<!-- wp:generateblocks/headline {"uniqueId":"703a 9bab","element":"p","blockVersion":3,"typography":{"fontSize":"16px","fontSizeMobile":"15px"},"textColor":"var(\u002d\u002dinsg-white)"} -->
+	<p class="gb-headline gb-headline-703a9bab gb-headline-text"> +17000 abonnés en profitent déjà</p>
 	<!-- /wp:generateblocks/headline --></div>
 	<!-- /wp:group -->
 	<!-- /wp:generateblocks/container -->
@@ -37,13 +38,13 @@ function insg_register_newsletter_block_pattern()
 CONTENT;
 
 	register_block_pattern(
-		'insg/newsletter-pattern',
+		'insg/bulletin-pattern',
 		array(
-			'title' => __('INSG. Newsletter', 'insg'),
-			'description' => _x('Custom Newsletter layout.', 'Block pattern description', 'insg'),
+			'title' => __('INSG. Bulletin', 'insg'),
+			'description' => _x('Custom Bulletin layout.', 'Block pattern description', 'insg'),
 			'content' => $content,
 		)
 	);
 }
 
-add_action('init', 'insg_register_newsletter_block_pattern');
+add_action('init', 'insg_register_bulletin_block_pattern');
